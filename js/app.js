e203a35e772caab43450963bdadad90d8dce7dac
Vanessa De La Cruz <delacruzvanessa445@gmail.com>
2021-04-27 14:48:31 -0700

    Adding courses into the shopping cart part1 part2

--- js/app.js
+++ js/app.js
@@ -45,3 +45,34 @@ function getCourseInfo(course)
 function getCourseInfo(course){
 
 }
+//Create an object with course data
+    const courseInfo = {
+         image:course.querySelector('img').src,
+        title:course.querySelector('h4').textContent,
+        price:course.querySelector('.price span').textContent,
+        id:course.querySelector('a').getAttribute('data-id')
+    }
+
+ //Insert into the shopping cart
+    addIntoCart(courseInfo);
+}
+//Display the Selected course into the shopping cart
+
+function addIntoCart(course){
+ //Create a <tr>
+    const row = document.createElement('tr');
+
+    //Build the template
+  row.innerHTML = ` 
+	 <tr>
+            <td>
+                <img src="${course.image}" width=100>
+            </td>
+            <td>${course.title}</td>
+            <td>${course.price}</td>
+            <td>
+                <a href="#" class="remove" data-id="${course.id}">X</a>
+            </td>
+        </tr>
+
+  `;
